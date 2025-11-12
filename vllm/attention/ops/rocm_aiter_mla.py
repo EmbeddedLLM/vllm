93446813517ca41b5dda927a2b42c73da72ee8c0
vllmellm <vllm.ellm@embeddedllm.com>
2025-11-12 14:29:23 +0000

    merge conflict bugfix
    Signed-off-by: vllmellm <vllm.ellm@embeddedllm.com>

--- vllm/attention/ops/rocm_aiter_mla.py
+++ vllm/attention/ops/rocm_aiter_mla.py
@@ -6,7 +6,7 @@ import torch
 
 from vllm.platforms import current_platform
 from vllm.triton_utils import tl, triton
-from vllm.utils import direct_register_custom_op, is_torch_equal_or_newer
+from vllm.utils.torch_utils import direct_register_custom_op, is_torch_equal_or_newer
 
 
 def get_aiter_mla_metadata(
